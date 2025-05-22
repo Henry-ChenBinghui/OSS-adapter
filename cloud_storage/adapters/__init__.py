6df60a5faaec499e252e11ca06e27917abb150f6
CaptainHui <120873604@qq.com>
2025-05-22 10:27:20 +0800

    update

--- cloud_storage/adapters/__init__.py
+++ cloud_storage/adapters/__init__.py
@@ -1,5 +1,13 @@
-from .aws import S3Storage
-from .azure import AzureBlobStorage
-from .gcp import GCSStorage
+__all__ = ['S3Storage', 'AzureBlobStorage', 'GCSStorage']
 
-__all__ = ['S3Storage', 'AzureBlobStorage', 'GCSStorage'] 
+def __getattr__(name):
+    if name == 'S3Storage':
+        from .aws import S3Storage
+        return S3Storage
+    elif name == 'AzureBlobStorage':
+        from .azure import AzureBlobStorage
+        return AzureBlobStorage
+    elif name == 'GCSStorage':
+        from .gcp import GCSStorage
+        return GCSStorage
+    raise AttributeError(f"module 'cloud_storage.adapters' has no attribute '{name}'") 
