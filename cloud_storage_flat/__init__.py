912368f19be84476216d0c6175f60bf5b3d4f2da
CaptainHui <120873604@qq.com>
2025-05-22 16:08:57 +0800

    update

--- cloud_storage_flat/__init__.py
+++ cloud_storage_flat/__init__.py
@@ -1,6 +1,15 @@
 from ._storage_base import CloudStorage
-from ._aws import S3Storage
-from ._azure import AzureBlobStorage
-from ._gcp import GCSStorage
+
+def __getattr__(name):
+    if name == 'S3Storage':
+        from ._aws import S3Storage
+        return S3Storage
+    elif name == 'AzureBlobStorage':
+        from ._azure import AzureBlobStorage
+        return AzureBlobStorage
+    elif name == 'GCSStorage':
+        from ._gcp import GCSStorage
+        return GCSStorage
+    raise AttributeError(f"module 'cloud_storage_flat' has no attribute '{name}'")
 
 __all__ = ['CloudStorage', 'S3Storage', 'AzureBlobStorage', 'GCSStorage'] 
